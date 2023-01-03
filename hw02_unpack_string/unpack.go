505e0cd9d84cc0af013aa39181eb0279edbf27f5
Pavel Sausvedris <psausvedris@gmail.com>
2023-01-03 01:28:27 +0000

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,61 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const EscapeRune rune = '\\'
+
+func isCanBeEscaped(r rune) bool {
+	return EscapeRune == r || unicode.IsDigit(r)
+}
+
+func Unpack(input string) (string, error) {
+
+	runes := []rune(input)
+	inputLen := len(runes)
+
+	if inputLen == 0 {
+		return "", nil
+	}
+
+	if unicode.IsDigit(runes[0]) {
+		return "", ErrInvalidString
+	}
+
+	var b strings.Builder
+	var index int = 0
+
+	for index < inputLen {
+		if runes[index] == EscapeRune {
+			index++
+		}
+
+		currentRune := runes[index]
+
+		if isCanBeEscaped(currentRune) && runes[index-1] != EscapeRune {
+			return "", ErrInvalidString
+		}
+
+		nextRuneIndex := index + 1
+		p := 1
+
+		if nextRuneIndex < inputLen && unicode.IsDigit(runes[nextRuneIndex]) {
+			p, _ = strconv.Atoi(string(runes[nextRuneIndex]))
+			index++
+		}
+
+		for p > 0 {
+			b.WriteRune(currentRune)
+			p--
+		}
+
+		index++
+	}
+
+	return b.String(), nil
 }
