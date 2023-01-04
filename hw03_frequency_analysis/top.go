97cbb387ab30321ee5d2625e564db65b4c69bcc2
Pavel Sausvedris <psausvedris@gmail.com>
2023-01-04 22:46:07 +0000

    HW03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,24 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"strings"
+)
+
+var cleanUpWordRegexp *regexp.Regexp = regexp.MustCompile(`[^a-zA-Z\\u0430-яА-Я-]+`)
+
+func Top10(text string) []string {
+	collector := NewWordCollector()
+
+	for _, value := range strings.Fields(text) {
+		value = strings.Trim(value, "-")
+		if len(value) == 0 {
+			continue
+		}
+
+		value := cleanUpWordRegexp.ReplaceAllString(value, "")
+		collector.wordUp(strings.ToLower(value))
+	}
+
+	return collector.GetWordFrenquencySorter().GetSortedWords(10)
 }
